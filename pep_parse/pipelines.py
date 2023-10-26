c78d06bddf3a9deeb02e9b22946ed2077e2d7ce8
Andrey Velikorechin <asvelikorechin@gmail.com>
2023-10-26 20:53:29 +0300

    After review

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -7,11 +7,10 @@ from pep_parse.settings import BASE_DIR, DATE_FROMAT
 class PepParsePipeline:
 
     def open_spider(self, spider):
-        self.statuses = defaultdict(int)
+        self.pep_statuses = defaultdict(int)
 
     def process_item(self, item, spider):
-        self.statuses[item['status']] += 1
-        print(self.statuses)
+        self.pep_statuses[item['status']] += 1
         return item
 
     def close_spider(self, spider):
@@ -23,6 +22,6 @@ class PepParsePipeline:
         file_path = results_dir / filename
         with open(file_path, mode='w', encoding='utf-8') as f:
             f.write('Статус,Количество\n')
-            for status, count in self.statuses.items():
+            for status, count in self.pep_statuses.items():
                 f.write(f'{status},{count}\n')
-            f.write(f'Total,{sum(self.statuses.values())}\n')
+            f.write(f'Total,{sum(self.pep_statuses.values())}\n')
